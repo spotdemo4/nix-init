17050fae25b8d2befaeec8dc5b3fe99683d20a39
spotdemo4 <me@trev.xyz>
2026-01-25 13:50:49 -0500

    fix: bump nix-nario-action

--- flake.nix
+++ flake.nix
@@ -41,22 +41,25 @@
       {
         devShells = {
           default = pkgs.mkShell {
+            name = "default";
+            shellHook = pkgs.shellhook.ref;
             packages = with pkgs; [
               bumper
 
               # nix
               nixfmt
             ];
-            shellHook = pkgs.shellhook.ref;
           };
 
           update = pkgs.mkShell {
+            name = "update";
             packages = with pkgs; [
               renovate
             ];
           };
 
           vulnerable = pkgs.mkShell {
+            name = "vulnerable";
             packages = with pkgs; [
               # nix
               flake-checker
